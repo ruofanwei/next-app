31b350bb6379819c08d5ed0bcb4fe87a8263c808
ruofan <tinawei@damaiapp.com.tw>
2021-11-04 16:12:56 +0800

    modify: tailwind config file

--- next.d.ts
+++ next.d.ts
@@ -1,2 +1,14 @@
 import { NextPageWithLayout } from 'next';
 import { AppProps } from 'next/app';
+
+declare module 'next' {
+  type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode;
+  };
+}
+
+declare module 'next/app' {
+  type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+  };
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
-// apps/site/tailwind.config.js
-const { join } = require("path");
-
 module.exports = {
-  purge: [
-    join(__dirname, "pages/**/*.{js,ts,jsx,tsx}"),
-  ],
-  mode: "jit",
-  darkMode: "media", // or 'media' or 'class'
+  purge: [join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}')],
+  mode: 'jit',
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
