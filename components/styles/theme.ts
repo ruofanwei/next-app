d62fcbc88b5b8f02ad08592a5fed1dde97a3a22e
ruofan <tinawei@damaiapp.com.tw>
2021-11-05 09:55:00 +0800

    chore: config for typescript and chakra

--- components/styles/theme.ts
+++ components/styles/theme.ts
@@ -1,15 +1,22 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 import { createBreakpoints } from '@chakra-ui/theme-tools';
 
 const breakpoints = createBreakpoints({
-  sm: '30em',
-  md: '48em',
-  lg: '62em',
-  xl: '80em',
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+  '2xl': '1536px',
 });
+
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
 
 const theme = extendTheme({
   breakpoints,
+  config,
 });
 
 export default theme;
