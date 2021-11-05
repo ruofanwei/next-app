7c5429804057fbae061bb85a000213a4aab7f51b
ruofan <tinawei@damaiapp.com.tw>
2021-11-05 14:36:58 +0800

    feat: footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import type { NextPage } from 'next';
 import { Container } from '../components/Container';
 import { Header } from '../components/Header/index';
 import { Footer } from '../components/Footer';
+import { Main } from '../components/Main';
 const Home: NextPage = () => {
   return (
     <Container height="100vh">
       <Header />
+      <Main />
       <Footer />
     </Container>
   );
