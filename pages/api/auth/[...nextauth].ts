d6e3bbbfcb36ddb771f5ec88363afe6a7c53ce22
ruofan <tinawei@damaiapp.com.tw>
2021-11-22 14:05:16 +0800

    feat: nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,11 @@
 import NextAuth from 'next-auth';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import LineProvider from 'next-auth/providers/line';
-export default NextAuth({
-  providers: [
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  const providers = [
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
@@ -26,5 +28,35 @@ export default NextAuth({
         id_token_signed_response_alg: 'HS256', // --> add this
       },
     }),
-  ],
-});
+  ];
+
+  return await NextAuth(req, res, {
+    providers,
+    // Enable debug messages in the console if you are having problems
+    debug: true,
+    callbacks: {
+      async signIn({ user, account, profile, email, credentials }) {
+        const isAllowedToSignIn = true;
+        if (isAllowedToSignIn) {
+          return true;
+        } else {
+          // Return false to display a default error message
+          return false;
+          // Or you can return a URL to redirect to:
+          // return '/unauthorized'
+        }
+      },
+      async session({ session, token, user }) {
+        // Send properties to the client, like an access_token from a provider.
+        session.accessToken = token.accessToken;
+        return session;
+      },
+      redirect({ url, baseUrl }) {
+        if (url.startsWith(baseUrl)) return url;
+        // Allows relative callback URLs
+        else if (url.startsWith('/')) return new URL(url, baseUrl).toString();
+        return baseUrl;
+      },
+    },
+  });
+}
